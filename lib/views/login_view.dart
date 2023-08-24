c0c840c68fe3cd4267571ec08e34f6e37cdbce51
Kouotou Ahmad Bilal <kouotouahmad@gmail.com>
2023-08-24 03:05:04 +0100

    Error handling in Login view

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,8 @@ import 'dart:developer' as devtools show log;
 
 import 'package:learnflutter/constants/routes.dart';
 
+import '../utilities/show_error_dialog.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
@@ -73,12 +75,29 @@ class _HomePageState extends State<LoginView> {
                 devtools.log(userCredential.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('User not found dear');
+                  await showErrorDialog(
+                    context,
+                    "User not found",
+                  );
+                  //devtools.log('User not found dear');
                 } else if (e.code == 'wrong-password') {
-                  devtools.log('Wrong password dear');
+                  await showErrorDialog(
+                    context,
+                    "Wrong Password",
+                  );
+                  //devtools.log('Wrong password dear');
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
                 }
               } catch (e) {
-                devtools.log('Something bad happened dear');
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
+                //devtools.log('Something bad happened dear');
               }
             },
             child: const Text('Login'),
